ec01c0446672b311b706aaed4cdaae8ce45e3cc8
Paul Zupan <paul.zupan.13@gmail.com>
2023-04-09 11:12:27 -0700

    Fix seg fault for nonexistent users

--- src/main.c
+++ src/main.c
@@ -1,58 +1,77 @@
-/* Error: seg fault
- * username = jim
- * password = 1234
- */
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define ACCOUNTS_SIZE 1
+#define PASSWORD_SIZE 64
+#define USERNAME_SIZE 64
 
 typedef struct {
     const char *username;
     const char *password;
 } Account;
 
+Account accounts[ACCOUNTS_SIZE] = {
+    {"paul", "1234"},
+};
+
+void get_password(char *buffer, size_t length);
+int get_user(Account *user, const char *username, const char *password);
+void get_username(char *buffer, size_t length);
 char *read_string(char *buffer, size_t length);
 
 int main(void)
 {
-    char username[64];
-    char password[64];
-
-    Account accounts[] = {
-        {"paul", "1234"},
-    };
+    char username[USERNAME_SIZE];
+    char password[PASSWORD_SIZE];
 
     size_t i;
 
-    printf("Enter your username: ");
-    if (read_string(username, sizeof(username)) == NULL) {
-        fprintf(stderr, "Failed to get username\n");
-        return 1;
-    }
-    printf("Enter your password: ");
-    if (read_string(password, sizeof(password)) == NULL) {
-        fprintf(stderr, "Failed to get password\n");
-        return 1;
-    }
-    printf("Authenticated:\n");
-    printf("==> Username '%s'\n", username);
-    printf("==> Password '%s'\n", password);
+    get_username(username, USERNAME_SIZE);
+    get_password(password, PASSWORD_SIZE);
 
-    for (i = 0; i < sizeof(accounts); ++i) {
-        Account account = accounts[i];
-        if (strcmp(username, account.username) == 0) {
-            if (strcmp(password, account.password) == 0) {
-                printf("Welcome, %s!\n", username);
-                return 0;
-            }
-            printf("Incorrect password!\n");
-            return 0;
+    for (i = 0; i < ACCOUNTS_SIZE; ++i) {
+        const Account *account = &accounts[i];
+
+        if (strcmp(username, account->username) != 0) {
+            continue;
         }
+
+        if (strcmp(password, account->password) != 0) {
+            printf("Incorrect password!\n");
+            break;
+        }
+
+        printf("Welcome, %s!\n", username);
     }
     printf("That user does not exist!\n");
 
     return 0;
 }
+
+void get_password(char *buffer, size_t length)
+{
+    printf("Enter your password: ");
+    if (read_string(buffer, length) == NULL) {
+        fprintf(stderr, "Failed to get password\n");
+        exit(1);
+    }
+}
+
+int get_user(Account *user, const char *username, const char *password)
+{
+    // ...
+}
+
+void get_username(char *buffer, size_t length)
+{
+    printf("Enter your username: ");
+    if (read_string(buffer, length) == NULL) {
+        fprintf(stderr, "Failed to get username\n");
+        exit(1);
+    }
+}
+
 
 char *read_string(char *buffer, size_t length)
 {
